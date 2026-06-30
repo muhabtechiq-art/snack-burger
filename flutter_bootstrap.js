8fd599562007f17b2ae640b330b357b30f76c2fd
muhabtechiq-art <muhabtechiq-art@users.noreply.github.com>
2026-06-30 16:07:56 +0000

    deploy: GitHub Pages (991a43f067b130422cf4b0d7a578d9386eb90d69) 991a43f067b130422cf4b0d7a578d9386eb90d69

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f8d","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc030","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 (function () {
